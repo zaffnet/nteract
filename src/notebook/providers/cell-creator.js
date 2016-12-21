9ae4cbbcc00bcf383a37f1ed3ee6084e8417da86
Peggy Rayzis <peggyrayzis@gmail.com>
2016-12-20 23:26:30 -0500

    refactor(provider): Removed Redux logic from cell creator & buttons

---
+++ src/notebook/providers/cell-creator.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import {
+  createCellAfter,
+  createCellAppend,
+  createCellBefore,
+  mergeCellAfter,
+} from '../actions';
+import CellCreatorView from '../views/cell-creator';
+
+type Props = {
+  above: boolean,
+  dispatch: Dispatch,
+  id: string|null,
+}
+
+class CellCreator extends Component {
+  props: Props;
+
+  constructor(): void {
+    super();
+
+    this.createCell = this.createCell.bind(this);
+    this.mergeCell = this.mergeCell.bind(this);
+  }
+
+  createCell(type: string): void {
+    const { dispatch, above, id } = this.props;
+
+    if (!id) {
+      dispatch(createCellAppend(type));
+      return;
+    }
+
+    if (above) {
+      dispatch(createCellBefore(type, id));
+    } else {
+      dispatch(createCellAfter(type, id));
+    }
+  }
+
+  mergeCell(): void {
+    const { dispatch, id } = this.props;
+
+    dispatch(mergeCellAfter(id));
+  }
+
+  render(): React.Element<any> {
+    const props: CellCreatorProps = {
+      ...this.props,
+      createCell: this.createCell,
+      mergeCell: this.mergeCell,
+    };
+
+    return (
+      <CellCreatorView {...props} />
+    );
+  }
+}
+
+export default connect()(CellCreator);
