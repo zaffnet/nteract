29b427243c674aa24713466ae16abd7aa2b27ab2
Kyle Kelley <rgbkrk@gmail.com>
2017-01-14 18:21:33 -0800

    chore(flow): Prefix config State with Config

--- src/notebook/reducers/config.js
+++ src/notebook/reducers/config.js
@@ -7,19 +7,19 @@ type MergeConfigAction = { type: 'MERGE_CONFIG', config: Map<any, any> };
 
 type ConfigAction = SetConfigAction | MergeConfigAction;
 
-type State = Map<any, any>;
+type ConfigState = Map<any, any>;
 
-export function setConfigKey(state: State, action: SetConfigAction) {
+export function setConfigKey(state: ConfigState, action: SetConfigAction) {
   const { key, value } = action;
   return state.set(key, value);
 }
 
-export function mergeConfig(state: State, action: MergeConfigAction) {
+export function mergeConfig(state: ConfigState, action: MergeConfigAction) {
   const { config } = action;
   return state.merge(config);
 }
 
-export default function handleConfig(state: State = Map(), action: ConfigAction) {
+export default function handleConfig(state: ConfigState = Map(), action: ConfigAction) {
   switch (action.type) {
     case 'SET_CONFIG_KEY':
       return setConfigKey(state, action);
