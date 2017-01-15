f4b9b0bbfc617463243de7aff2663ee29555155a
Kyle Kelley <rgbkrk@gmail.com>
2017-01-14 18:01:45 -0800

    chore(flow): fully type config.js

--- src/notebook/reducers/config.js
+++ src/notebook/reducers/config.js
@@ -1,14 +1,31 @@
-import { handleActions } from 'redux-actions';
+/* @flow */
 
-import * as constants from '../constants';
+import { Map } from 'immutable';
 
-export default handleActions({
-  [constants.SET_CONFIG_KEY]: function setConfigKey(state, action) {
-    const { key, value } = action;
-    return state.set(key, value);
-  },
-  [constants.MERGE_CONFIG]: function mergeConfig(state, action) {
-    const { config } = action;
-    return state.merge(config);
-  },
-}, {});
+type SetConfigAction = { type: 'SET_CONFIG_KEY', key: string, value: string };
+type MergeConfigAction = { type: 'MERGE_CONFIG', config: Map<any, any> };
+
+type ConfigAction = SetConfigAction | MergeConfigAction;
+
+type State = Map<any, any>;
+
+export function setConfigKey(state: State, action: SetConfigAction) {
+  const { key, value } = action;
+  return state.set(key, value);
+}
+
+export function mergeConfig(state: State, action: MergeConfigAction) {
+  const { config } = action;
+  return state.merge(config);
+}
+
+export default function handleConfig(state: State = Map(), action: ConfigAction) {
+  switch (action.type) {
+    case 'SET_CONFIG_KEY':
+      return setConfigKey(state, action);
+    case 'MERGE_CONFIG':
+      return mergeConfig(state, action);
+    default:
+      return state;
+  }
+}
