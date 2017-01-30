43cdb928bfc808acc2c7efc41fc5084e32ffbae9
Kyle Kelley <rgbkrk@gmail.com>
2017-01-30 13:26:16 -0600

    flow(metadata): type annotate the metadata reducer (#1382)

--- src/notebook/reducers/metadata.js
+++ src/notebook/reducers/metadata.js
@@ -1,15 +1,25 @@
-import { handleActions } from 'redux-actions';
+/* @flow */
+import * as Immutable from 'immutable';
 
-import * as constants from '../constants';
+type MetadataState = Immutable.Map<string, any>;
+const defaultMetadataState : MetadataState = new Immutable.Map({
+  filename: null,
+});
 
-function changeFilename(state, action) {
+type ChangeFilenameAction = { type: 'CHANGE_FILENAME' | 'SET_NOTEBOOK', filename: string };
+function changeFilename(state: MetadataState, action: ChangeFilenameAction) {
   if (action.filename) {
     return state.set('filename', action.filename);
   }
   return state;
 }
 
-export default handleActions({
-  [constants.CHANGE_FILENAME]: changeFilename,
-  [constants.SET_NOTEBOOK]: changeFilename,
-}, {});
+export default function (
+  state: MetadataState = defaultMetadataState,
+  action: ChangeFilenameAction
+) {
+  if (action.type === 'CHANGE_FILENAME' || action.type === 'SET_NOTEBOOK') {
+    return changeFilename(state, action);
+  }
+  return state;
+}
