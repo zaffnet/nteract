3a4db7d8539cd4c494cdedcfc561bea3ded46ec6
Grant Nestor <grantnestor@gmail.com>
2017-03-10 08:21:39 -0800

    Estimate columns widths from a sample of column values

--- packages/transform-dataresource/src/virtualized-grid.js
+++ packages/transform-dataresource/src/virtualized-grid.js
@@ -11,6 +11,7 @@ const GRID_MAX_HEIGHT = ROW_HEIGHT * 10;
 // The width per text character for calculating widths for columns
 const COLUMN_CHARACTER_WIDTH = 7;
 // The number of sample rows that should be used to infer types for columns
+// and widths for columns
 const SAMPLE_SIZE = 10;
 
 type Props = {
@@ -22,6 +23,7 @@ type Props = {
 type State = {
   data: Array<Object>,
   schema: { fields: Array<Object> },
+  columnWidths: Array<number>
 };
 
 function getSampleRows(data: Array<Object>, sampleSize: number): Array<Object> {
@@ -54,9 +56,18 @@ function getState(props: Props) {
     (result, column) => ({ ...result, [column]: column }),
     {}
   );
+  const columnWidths = columns.map(column => {
+    const sampleRows = getSampleRows(data, SAMPLE_SIZE);
+    return sampleRows.reduce(
+      (result, row) =>
+        `${row[column]}`.length > result ? `${row[column]}`.length : result,
+      Math.ceil(COLUMN_WIDTH / COLUMN_CHARACTER_WIDTH)
+    );
+  });
   return {
     data: [headers, ...data],
     schema,
+    columnWidths
   };
 }
 
@@ -65,6 +76,7 @@ export default class VirtualizedGrid extends React.Component {
   state: State = {
     data: [],
     schema: { fields: [] },
+    columnWidths: []
   };
 
   componentWillMount() {
@@ -137,7 +149,9 @@ export default class VirtualizedGrid extends React.Component {
           <MultiGrid
             cellRenderer={this.cellRenderer}
             columnCount={this.state.schema.fields.length}
-            columnWidth={COLUMN_WIDTH}
+            columnWidth={({ index }) =>
+              this.state.columnWidths[index] * COLUMN_CHARACTER_WIDTH ||
+              COLUMN_WIDTH}
             fixedColumnCount={1}
             fixedRowCount={1}
             height={height < GRID_MAX_HEIGHT ? height : GRID_MAX_HEIGHT}
