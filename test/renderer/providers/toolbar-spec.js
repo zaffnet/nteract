b47ba61f141e099a98e62902ef43af79adbb2fb2
John Detlefs <jdetle@gmail.com>
2017-03-22 09:22:35 -0600

    test(provider): Got tests for provider working.

--- test/renderer/providers/toolbar-spec.js
+++ test/renderer/providers/toolbar-spec.js
@@ -6,16 +6,120 @@ import { Provider } from 'react-redux';
 import Toolbar from '../../../src/notebook/providers/toolbar';
 import { dummyStore } from '../../utils';
 
+import {
+  TOGGLE_STICKY_CELL,
+  REMOVE_CELL,
+  EXECUTE_CELL,
+  CLEAR_OUTPUTS,
+  CHANGE_INPUT_VISIBILITY,
+  CHANGE_OUTPUT_VISIBILITY,
+  CHANGE_CELL_TYPE,
+  TOGGLE_OUTPUT_EXPANSION,
+} from '../../../src/notebook/constants';
+
 describe('toolbar provider', () => {
   const store = dummyStore();
+  const dropdown = { hide: () => {} }
 
-  const setup = (type, id) => mount(
+  const setup = (props) => mount(
     <Provider store={store}>
-      <Toolbar type={type} id={id} />
+      <Toolbar {...props} />
     </Provider>,
     );
 
   it('is mountable', () => {
     const toolbar = setup('code', 'cell');
   })
+
+  it('toggle Sticky Cell works', (done) => {
+    const dispatch = (action) => {
+      expect(action.id).to.equal('cell');
+      expect(action.type).to.equal(TOGGLE_STICKY_CELL);
+      done();
+    };
+    store.dispatch = dispatch;
+    const toolbar = setup({id: 'cell'});
+    toolbar.find('Toolbar').node.toggleStickyCell()
+  });
+
+  it('removes cell works', (done) => {
+    const dispatch = (action) => {
+      expect(action.id).to.equal('cell');
+      expect(action.type).to.equal(REMOVE_CELL);
+      done();
+    };
+    store.dispatch = dispatch;
+    const toolbar = setup({type: 'code', id: 'cell'});
+    toolbar.find('Toolbar').node.removeCell()
+  });
+
+  it('execute cell works', (done) => {
+    const dispatch = (action) => {
+      expect(action.id).to.equal('cell');
+      expect(action.source).to.equal('source')
+      expect(action.type).to.equal(EXECUTE_CELL);
+      done();
+    };
+    store.dispatch = dispatch;
+    const func = (args) => { return 'source' };
+    const cell = { get: func };
+    const toolbar = setup({ id: 'cell', cell });
+    toolbar.find('Toolbar').node.executeCell()
+  });
+
+  it('clear outputs works', (done) => {
+    const dispatch = (action) => {
+      expect(action.id).to.equal('cell');
+      expect(action.type).to.equal(CLEAR_OUTPUTS);
+      done();
+    };
+    store.dispatch = dispatch;
+    const toolbar = setup({ id: 'cell' });
+    toolbar.find('Toolbar').node.clearOutputs(dropdown)
+  });
+
+  it('change Input Visibility works', (done) => {
+    const dispatch = (action) => {
+      expect(action.id).to.equal('cell');
+      expect(action.type).to.equal(CHANGE_INPUT_VISIBILITY);
+      done();
+    };
+    store.dispatch = dispatch;
+    const toolbar = setup({ id: 'cell' });
+    toolbar.find('Toolbar').node.changeInputVisibility(dropdown)
+  });
+
+  it('change Output Visibility works', (done) => {
+    const dispatch = (action) => {
+      expect(action.id).to.equal('cell');
+      expect(action.type).to.equal(CHANGE_OUTPUT_VISIBILITY);
+      done();
+    };
+    store.dispatch = dispatch;
+    const toolbar = setup({ id: 'cell' });
+    toolbar.find('Toolbar').node.changeOutputVisibility(dropdown)
+  });
+
+  it('change Cell Type works', (done) => {
+    const dispatch = (action) => {
+      expect(action.id).to.equal('cell');
+      expect(action.to).to.equal('markdown');
+      expect(action.type).to.equal(CHANGE_CELL_TYPE);
+      done();
+    };
+    store.dispatch = dispatch;
+    const toolbar = setup({ id: 'cell', type: 'code' });
+    toolbar.find('Toolbar').node.changeCellType(dropdown)
+  });
+
+  it('toggle output expansion works', (done) => {
+    const dispatch = (action) => {
+      expect(action.id).to.equal('cell');
+      expect(action.type).to.equal(TOGGLE_OUTPUT_EXPANSION);
+      done();
+    };
+    store.dispatch = dispatch;
+    const toolbar = setup({ id: 'cell' });
+    toolbar.find('Toolbar').node.toggleOutputExpansion(dropdown)
+  });
 })
