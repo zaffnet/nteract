1ba442dba6e408745d226ee0c5d25e3198c3e4e9
Peggy Rayzis <peggyrayzis@gmail.com>
2017-04-04 21:09:24 -0400

    chore(webpack): Upgrade current build to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-const webpack = require('webpack');
-const path = require('path');
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
 
 const nodeModules = {
-  zmq: 'commonjs zmq',
-  jmp: 'commonjs jmp',
-  github: 'commonjs github',
-  canvas: 'commonjs canvas',
+  zmq: "commonjs zmq",
+  jmp: "commonjs jmp",
+  github: "commonjs github",
+  canvas: "commonjs canvas"
 };
 
 module.exports = {
-  entry: './src/notebook/index.js',
-  target: 'electron-renderer',
+  entry: "./src/notebook/index.js",
+  target: "electron-renderer",
   output: {
-    path: path.join(__dirname, 'lib'),
-    filename: 'webpacked-notebook.js'
+    path: path.join(__dirname, "lib"),
+    filename: "webpacked-notebook.js"
   },
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.json$/, loader: 'json-loader' },
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.json$/, loader: "json-loader" }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: [".js", ".jsx"]
   },
   externals: nodeModules,
   plugins: [
     new LodashModuleReplacementPlugin(),
-    new webpack.IgnorePlugin(/\.(css|less)$/),
-  ],
+    new webpack.IgnorePlugin(/\.(css|less)$/)
+  ]
 };
