410ec9c54083848b9dde43b119344d270e1d5a4d
Lukas Geiger <lukas.geiger94@gmail.com>
2017-09-14 04:13:58 +0200

    Convert ansi-to-react tests to jest
    Using jest-codemods

--- packages/ansi-to-react/__tests__/index-spec.js
+++ packages/ansi-to-react/__tests__/index-spec.js
@@ -1,6 +1,5 @@
 const Ansi = require("../src/index");
 const React = require("react");
-const expect = require("chai").expect;
 const enzyme = require("enzyme");
 
 const GREEN_FG = "\u001b[32m";
@@ -8,35 +7,35 @@ const YELLOW_BG = "\u001b[43m";
 const RESET = "\u001b[0;m";
 
 describe("Ansi", () => {
-  it("hello world", () => {
+  test("hello world", () => {
     const el = enzyme.shallow(React.createElement(Ansi, null, "hello world"));
-    expect(el).to.not.be.null;
-    expect(el.text()).to.equal("hello world");
+    expect(el).not.toBeNull();
+    expect(el.text()).toBe("hello world");
   });
 
-  it("can color", () => {
+  test("can color", () => {
     const el = enzyme.shallow(
       React.createElement(Ansi, null, `hello ${GREEN_FG}world`)
     );
-    expect(el).to.not.be.null;
-    expect(el.text()).to.equal("hello world");
-    expect(el.html()).to.equal(
+    expect(el).not.toBeNull();
+    expect(el.text()).toBe("hello world");
+    expect(el.html()).toBe(
       '<code><span>hello </span><span style="color:rgb(0, 187, 0);">world</span></code>'
     );
   });
 
-  it("can have className", () => {
+  test("can have className", () => {
     const el = enzyme.shallow(
       React.createElement(Ansi, { className: "my-class" }, `hello world`)
     );
-    expect(el).to.not.be.null;
-    expect(el.text()).to.equal("hello world");
-    expect(el.html()).to.equal(
+    expect(el).not.toBeNull();
+    expect(el.text()).toBe("hello world");
+    expect(el.html()).toBe(
       '<code class="my-class"><span>hello world</span></code>'
     );
   });
 
-  it("can nest", () => {
+  test("can nest", () => {
     const el = enzyme.shallow(
       React.createElement(
         Ansi,
@@ -44,14 +43,14 @@ describe("Ansi", () => {
         `hello ${GREEN_FG}wo${YELLOW_BG}rl${RESET}d`
       )
     );
-    expect(el).to.not.be.null;
-    expect(el.text()).to.equal("hello world");
-    expect(el.html()).to.equal(
+    expect(el).not.toBeNull();
+    expect(el.text()).toBe("hello world");
+    expect(el.html()).toBe(
       '<code><span>hello </span><span style="color:rgb(0, 187, 0);">wo</span><span style="background-color:rgb(187, 187, 0);color:rgb(0, 187, 0);">rl</span><span>d</span></code>'
     );
   });
 
-  it("can handle carriage symbol", () => {
+  test("can handle carriage symbol", () => {
     const el = enzyme.shallow(
       React.createElement(
         Ansi,
@@ -59,11 +58,11 @@ describe("Ansi", () => {
         "this sentence\rthat\nwill make you pause"
       )
     );
-    expect(el).to.not.be.null;
-    expect(el.text()).to.equal("that sentence\nwill make you pause");
+    expect(el).not.toBeNull();
+    expect(el.text()).toBe("that sentence\nwill make you pause");
   });
 
-  it("can linkify", () => {
+  test("can linkify", () => {
     const el = enzyme.shallow(
       React.createElement(
         Ansi,
@@ -71,14 +70,14 @@ describe("Ansi", () => {
         "this is a link: https://nteract.io/"
       )
     );
-    expect(el).to.not.be.null;
-    expect(el.text()).to.equal("this is a link: https://nteract.io/");
-    expect(el.html()).to.equal(
+    expect(el).not.toBeNull();
+    expect(el.text()).toBe("this is a link: https://nteract.io/");
+    expect(el.html()).toBe(
       '<code><span>this is a link: <a href="https://nteract.io/" target="_blank">https://nteract.io/</a></span></code>'
     );
   });
 
-  it("can distinguish URL-ish text", () => {
+  test("can distinguish URL-ish text", () => {
     const el = enzyme.shallow(
       React.createElement(
         Ansi,
@@ -86,11 +85,11 @@ describe("Ansi", () => {
         "<transport.model.TransportInfo"
       )
     );
-    expect(el).to.not.be.null;
-    expect(el.text()).to.equal("<transport.model.TransportInfo");
+    expect(el).not.toBeNull();
+    expect(el.text()).toBe("<transport.model.TransportInfo");
   });
 
-  it("can distinguish URL-ish text", () => {
+  test("can distinguish URL-ish text", () => {
     const el = enzyme.shallow(
       React.createElement(
         Ansi,
@@ -98,8 +97,8 @@ describe("Ansi", () => {
         "<module 'something' from '/usr/local/lib/python2.7/dist-packages/something/__init__.pyc'>"
       )
     );
-    expect(el).to.not.be.null;
-    expect(el.text()).to.equal(
+    expect(el).not.toBeNull();
+    expect(el.text()).toBe(
       "<module 'something' from '/usr/local/lib/python2.7/dist-packages/something/__init__.pyc'>"
     );
   });
