06d09e0772722259a91955f4501683df12dab685
Kyle Kelley <rgbkrk@gmail.com>
2017-10-03 20:32:10 -0700

    only run updateAndNotify on windows + macOS

--- packages/desktop/src/main/auto-updater.js
+++ packages/desktop/src/main/auto-updater.js
@@ -1,8 +1,11 @@
 import { autoUpdater } from "electron-updater";
 
 export function initAutoUpdater() {
-  const log = require("electron-log");
-  log.transports.file.level = "info";
-  autoUpdater.logger = log;
-  autoUpdater.checkForUpdatesAndNotify();
+  // Autoupdate should only get called on macOS and Windows
+  if (process.platform === "darwin" || process.platform === "win32") {
+    const log = require("electron-log");
+    log.transports.file.level = "info";
+    autoUpdater.logger = log;
+    autoUpdater.checkForUpdatesAndNotify();
+  }
 }
