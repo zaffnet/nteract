2af9c9b8f49a95c7a12dc65b1584e3c92cfd5b91
Kyle Kelley <rgbkrk@gmail.com>
2017-10-07 12:05:26 -0700

    remove pointless test for constants
    we're using flow so we don't need this kind of boilerplate check

--- packages/desktop/__tests__/renderer/constants-spec.js
+++
@@ -1,9 +0,0 @@
-import * as constants from "../../src/notebook/constants";
-
-describe("constants", () => {
-  Object.keys(constants).forEach(name => {
-    test(`${name} is not undefined`, () => {
-      expect(constants[name]).toBeDefined();
-    });
-  });
-});
