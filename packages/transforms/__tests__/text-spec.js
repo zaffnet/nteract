b90e319c8db9b2da6e2294b71dd943939d662b87
Kyle Kelley <rgbkrk@gmail.com>
2017-10-12 22:59:15 -0700

    remove sCU test for Text component

--- packages/transforms/__tests__/text-spec.js
+++ packages/transforms/__tests__/text-spec.js
@@ -7,10 +7,7 @@ import Text from "../src/text";
 describe("Text", () => {
   it("renders plain text", () => {
     const wrapper = mount(<Text data={"hey"} />);
+    // TODO: This test should be replaced with jest snapshots
     expect(wrapper.html()).toEqual("<code><span>hey</span></code>");
-
-    const component = wrapper.instance();
-
-    expect(component.shouldComponentUpdate()).toEqual(true);
   });
 });
