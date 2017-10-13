3e177e5a60792d5d751c64dc913004c21e65fb86
Lukas Geiger <lukas.geiger94@gmail.com>
2017-10-13 17:42:19 +0200

    Enable auto-updates on Linux AppImage

--- packages/desktop/src/main/auto-updater.js
+++ packages/desktop/src/main/auto-updater.js
@@ -1,11 +1,8 @@
 import { autoUpdater } from "electron-updater";
 
 export function initAutoUpdater() {
-  // Autoupdate should only get called on macOS and Windows
-  if (process.platform === "darwin" || process.platform === "win32") {
-    const log = require("electron-log");
-    log.transports.file.level = "info";
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
-  }
+  const log = require("electron-log");
+  log.transports.file.level = "info";
+  autoUpdater.logger = log;
+  autoUpdater.checkForUpdatesAndNotify();
 }
