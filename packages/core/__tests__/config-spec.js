717489b5d1507dd33faca30b0188273225fda4e6
Kyle Kelley <rgbkrk@gmail.com>
2017-11-01 16:37:08 -0700

    convert the config tests

--- packages/core/__tests__/config-spec.js
+++ packages/core/__tests__/config-spec.js
@@ -1,30 +1,30 @@
 import { Map } from "immutable";
 
 import * as constants from "../src/constants";
-import reducers from "../src/reducers";
+import { config as reducers } from "../src/reducers";
 
 describe("setKey", () => {
   test("sets the keys in the config", () => {
-    const initialState = { config: new Map({ theme: null }) };
+    const initialState = new Map({ theme: null });
 
     const state = reducers(initialState, {
       type: constants.SET_CONFIG_KEY,
       key: "theme",
       value: "light"
     });
-    expect(state.config.get("theme")).toBe("light");
+    expect(state.get("theme")).toBe("light");
   });
 });
 
 describe("mergeConfig", () => {
   test("sets the config", () => {
-    const initialState = { config: new Map() };
+    const initialState = new Map();
 
     const config = { theme: "dark" };
     const state = reducers(initialState, {
       type: constants.MERGE_CONFIG,
       config
     });
-    expect(state.config.get("theme")).toBe("dark");
+    expect(state.get("theme")).toBe("dark");
   });
 });
