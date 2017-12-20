9664ab06434623ace1abb1f270dc5f51943b7ad5
M Pacer <mpacer@berkeley.edu>
2017-12-19 18:07:16 -0800

    WIP working on server reducers

--- applications/play/redux/reducer.js
+++ applications/play/redux/reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import * as actionTypes from "./actionTypes";
+import { formBinderURL } from "rx-binder";
 
 const repo = (state = "binder-examples/jupyter-stacks", { type, payload }) => {
   switch (type) {
@@ -79,6 +80,83 @@ const setCurrentKernelName = (state = "", { type, payload }) => {
     default: {
       return state;
     }
+  }
+};
+
+const server = (state = {}, { type, payload }) => {
+  switch (type) {
+    case actionTypes.FETCH_SERVER: {
+      return { ...state, isFetching: true, error: null };
+    }
+    case actionTypes.FETCH_SERVER_FULFILLED:
+    case actionTypes.SET_SERVER:
+    case actionTypes.FETCH_SERVER_FAILED:
+    case actionTypes.FETCH_SERVER_CANCELED:
+    case actionTypes.KILL_SERVER:
+    case actionTypes.KILL_SERVER_FULFILLED:
+    case actionTypes.KILL_SERVER_FAILED:
+    case actionTypes.KILL_SERVER_CANCELED:
+    case actionTypes.FETCH_KERNEL_SPECS:
+    case actionTypes.FETCH_KERNEL_SPECS_FULFILLED:
+    case actionTypes.FETCH_KERNEL_SPECS_FAILED:
+    case actionTypes.FETCH_KERNEL_SPECS_CANCELED:
+    case actionTypes.SET_ACTIVE_KERNEL:
+    case actionTypes.SET_RESTART_KERNEL:
+    case actionTypes.ACTIVATE_KERNEL:
+    case actionTypes.ACTIVATE_KERNEL_FULFILLED:
+    case actionTypes.ACTIVATE_KERNEL_FAILED:
+    case actionTypes.ACTIVATE_KERNEL_CANCELED:
+    case actionTypes.INTERRUPT_KERNEL:
+    case actionTypes.INTERRUPT_KERNEL_FULFILLED:
+    case actionTypes.INTERRUPT_KERNEL_FAILED:
+    case actionTypes.INTERRUPT_KERNEL_CANCELED:
+    case actionTypes.KILL_KERNEL:
+    case actionTypes.KILL_KERNEL_FULFILLED:
+    case actionTypes.KILL_KERNEL_FAILED:
+    case actionTypes.KILL_KERNEL_CANCELED: {
+    }
+    default:
+      return state;
+  }
+};
+
+const serversById = (state = {}, { type, payload }) => {
+  switch (type) {
+    case actionTypes.FETCH_SERVER:
+    case actionTypes.FETCH_SERVER_FULFILLED:
+    case actionTypes.SET_SERVER:
+    case actionTypes.FETCH_SERVER_FAILED:
+    case actionTypes.FETCH_SERVER_CANCELED:
+    case actionTypes.KILL_SERVER:
+    case actionTypes.KILL_SERVER_FULFILLED:
+    case actionTypes.KILL_SERVER_FAILED:
+    case actionTypes.KILL_SERVER_CANCELED:
+    case actionTypes.FETCH_KERNEL_SPECS:
+    case actionTypes.FETCH_KERNEL_SPECS_FULFILLED:
+    case actionTypes.FETCH_KERNEL_SPECS_FAILED:
+    case actionTypes.FETCH_KERNEL_SPECS_CANCELED:
+    case actionTypes.SET_ACTIVE_KERNEL:
+    case actionTypes.SET_RESTART_KERNEL:
+    case actionTypes.ACTIVATE_KERNEL:
+    case actionTypes.ACTIVATE_KERNEL_FULFILLED:
+    case actionTypes.ACTIVATE_KERNEL_FAILED:
+    case actionTypes.ACTIVATE_KERNEL_CANCELED:
+    case actionTypes.INTERRUPT_KERNEL:
+    case actionTypes.INTERRUPT_KERNEL_FULFILLED:
+    case actionTypes.INTERRUPT_KERNEL_FAILED:
+    case actionTypes.INTERRUPT_KERNEL_CANCELED:
+    case actionTypes.KILL_KERNEL:
+    case actionTypes.KILL_KERNEL_FULFILLED:
+    case actionTypes.KILL_KERNEL_FAILED:
+    case actionTypes.KILL_KERNEL_CANCELED: {
+      // TODO: add flow type to indicate that id is required
+      return {
+        ...state,
+        [payload.id]: server(state[payload.id], { type, payload })
+      };
+    }
+    default:
+      return source;
   }
 };
 
