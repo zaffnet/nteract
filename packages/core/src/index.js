d77651b7a2b157d2aff3b256f526ec3d91af0125
Kyle Kelley <rgbkrk@gmail.com>
2018-01-01 14:10:08 -0800

    make themes behave like other modules inside core

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -5,5 +5,14 @@ import * as middlewares from "./middlewares";
 import * as reducers from "./reducers";
 import * as components from "./components";
 import * as providers from "./providers";
+import * as themes from "./themes";
 
-export { actions, constants, middlewares, reducers, components, providers };
+export {
+  actions,
+  constants,
+  middlewares,
+  reducers,
+  components,
+  providers,
+  themes
+};
