ba5f4ff09740177d49e0830689796cc4bf293956
Kyle Kelley <rgbkrk@gmail.com>
2018-01-03 11:12:46 -0800

    perform prefetching of mybinder DNS + logo

--- applications/play/components/Main.js
+++ applications/play/components/Main.js
@@ -1,5 +1,7 @@
 // @flow
 import * as React from "react";
+import Head from "next/head";
+
 import CodeMirrorEditor from "@nteract/editor";
 import { BinderConsole } from "./consoles";
 import { Display } from "@nteract/display-area";
@@ -77,6 +79,13 @@ class Main extends React.Component<*, *> {
     const { repoValue, gitrefValue, sourceValue } = this.state;
     return (
       <div>
+        <Head>
+          <link rel="dns-prefetch" href="https://mybinder.org" />
+          <link
+            rel="prefetch"
+            href="https://mybinder.org/static/logo.svg?v=f9f0d927b67cc9dc99d788c822ca21c0"
+          />
+        </Head>
         <header>
           <div className="left">
             <img
