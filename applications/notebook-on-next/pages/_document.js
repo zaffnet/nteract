75a6dc7313b8d8d93c9c33cd0eabccd59c2fc89c
Kyle Kelley <rgbkrk@gmail.com>
2018-01-11 10:54:43 -0800

    address the flow issues in notebook-on-next

--- applications/notebook-on-next/pages/_document.js
+++ applications/notebook-on-next/pages/_document.js
@@ -3,7 +3,7 @@ import React from "react";
 import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage: Function }) {
+  static getInitialProps({ renderPage }: { renderPage: Function }) {
     const { html, head, errorHtml, chunks } = renderPage();
     return { html, head, errorHtml, chunks };
   }
