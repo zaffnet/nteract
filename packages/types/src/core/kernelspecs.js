1ab1bfecdb22737e89ec9974e3d010e1b82a7a73
Andrew Seier <andseier@gmail.com>
2018-01-19 14:56:24 -0800

    feat: add new kernelspecs types.

--- packages/types/src/core/kernelspecs.js
+++ packages/types/src/core/kernelspecs.js
@@ -1,3 +1,55 @@
+import type { RecordFactory, RecordOf } from "immutable";
 import type { Ref } from "./ref";
+import { List, Map, Record } from "immutable";
 
 export opaque type KernelspecsRef = Ref;
+
+type CommunicationKernelspecsProps = {
+  loading: boolean,
+  error: ?Object
+};
+
+export type CommunicationKernelspecsRecord = RecordOf<
+  CommunicationKernelspecsProps
+>;
+
+export const communicationKernelspecsFactory: RecordFactory<
+  CommunicationKernelspecsProps
+> = Record({
+  loading: false,
+  error: null
+});
+
+type EntitiesKernelspecsSpecProps = {
+  language: string,
+  argv: List<string>,
+  env: Map
+};
+
+export type EntitiesKernelspecsSpecRecord = RecordOf<
+  EntitiesKernelspecsSpecProps
+>;
+
+export const entitiesKernelspecsSpecFactory: RecordFactory<
+  EntitiesKernelspecsSpecProps
+> = Record({
+  language: "", // TODO: what's a reasonable default?
+  argv: List(),
+  env: Map()
+});
+
+type EntitiesKernelspecsProps = {
+  name: string,
+  resources: Map,
+  spec: RecordOf<EntitiesKernelspecsSpecProps>
+};
+
+export type EntitiesKernelspecsRecord = RecordOf<EntitiesKernelspecsProps>;
+
+export const entitiesKernelspecsFactory: RecordFactory<
+  EntitiesKernelspecsProps
+> = Record({
+  name: "", // TODO: what's a reasonable default?
+  resources: Map(),
+  spec: entitiesKernelspecsSpecFactory()
+});
