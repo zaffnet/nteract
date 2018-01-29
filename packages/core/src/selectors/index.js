366a4b22f16b009b0f82f062b6820e576c616fae
Kyle Kelley <rgbkrk@gmail.com>
2018-01-29 10:38:53 -0800

    perform deeper selection on host properties

--- packages/core/src/selectors/index.js
+++ packages/core/src/selectors/index.js
@@ -3,10 +3,15 @@ import type { AppState } from "@nteract/types/core/records";
 
 import { createSelector } from "reselect";
 
-export const getHost = (state: AppState) => state.app.host;
+const serverUrl = (state: AppState) => state.app.host.serverUrl;
+const crossDomain = (state: AppState) => state.app.host.crossDomain;
+const token = (state: AppState) => state.app.host.token;
 
-export const getServerConfig = createSelector(getHost, host => ({
-  endpoint: host.serverUrl,
-  crossDomain: host.crossDomain,
-  token: host.token
-}));
+export const getServerConfig = createSelector(
+  [serverUrl, crossDomain, token],
+  (serverUrl, crossDomain, token) => ({
+    endpoint: serverUrl,
+    crossDomain,
+    token
+  })
+);
