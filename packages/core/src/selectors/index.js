1c951826a0e1345b3299d72163beaa91fdae8677
Andrew Seier <andseier@gmail.com>
2018-02-07 14:32:30 -0800

    Add a bunch of new selectors to `core`.

--- packages/core/src/selectors/index.js
+++ packages/core/src/selectors/index.js
@@ -1,8 +1,10 @@
 // @flow
 import type { AppState } from "@nteract/types/core/records";
+import { toJS, stringifyNotebook } from "@nteract/commutable";
 
 import { createSelector } from "reselect";
 
+const identity = thing => thing;
 const serverUrl = (state: AppState) => state.app.host.serverUrl;
 const crossDomain = (state: AppState) => state.app.host.crossDomain;
 const token = (state: AppState) => state.app.host.token;
@@ -16,18 +18,28 @@ export const serverConfig = createSelector(
   })
 );
 
+export const userPreferences = createSelector(
+  (state: AppState) => state.config,
+  config => config.toJS()
+);
+
+export const appVersion = createSelector(
+  (state: AppState) => state.app.version,
+  identity
+);
+
 // Quick memoized host and kernel selection.
 //
 // Intended to be useful for a core app and be future proof for when we have
 // both refs and selected/current hosts and kernels
 export const currentHost = createSelector(
   (state: AppState) => state.app.host,
-  host => host
+  identity
 );
 
 export const currentKernel = createSelector(
   (state: AppState) => state.app.kernel,
-  kernel => kernel
+  identity
 );
 
 export const currentKernelType = createSelector([currentKernel], kernel => {
@@ -61,4 +73,76 @@ export const isCurrentKernelJupyterWebsocket = createSelector(
   (hostType, kernelType) => {
     return hostType === "jupyter" && kernelType === "websocket";
   }
+);
+
+// TODO: if we're not looking at a notebook in the UI, there may not _be_ a
+// notebook object to get. Do we return null? Throw an error?
+export const currentNotebook = createSelector(
+  (state: AppState) => state.document.get("notebook"),
+  identity
+);
+
+export const currentSavedNotebook = createSelector(
+  (state: AppState) => state.document.get("savedNotebook"),
+  identity
+);
+
+export const currentNotebookGithubUsername = createSelector(
+  [currentNotebook],
+  notebook => notebook.getIn(["metadata", "github_username"])
+);
+
+export const currentNotebookGistId = createSelector(
+  [currentNotebook],
+  notebook => notebook.getIn(["metadata", "gist_id"])
+);
+
+export const currentNotebookJS = createSelector([currentNotebook], notebook =>
+  toJS(notebook)
+);
+
+export const currentNotebookString = createSelector(
+  [currentNotebookJS],
+  notebookJS => stringifyNotebook(notebookJS)
+);
+
+export const currentFocusedCellId = createSelector(
+  (state: AppState) => state.document.get("cellFocused"),
+  identity
+);
+
+export const currentCellMap = createSelector([currentNotebook], notebook =>
+  notebook.get("cellMap")
+);
+
+export const currentCellOrder = createSelector([currentNotebook], notebook =>
+  notebook.get("cellOrder")
+);
+
+export const currentCodeCellIds = createSelector(
+  [currentCellMap, currentCellOrder],
+  (cellMap, cellOrder) => {
+    return cellOrder.filter(id => cellMap.getIn([id, "cell_type"]) === "code");
+  }
+);
+
+export const currentCodeCellIdsBelow = createSelector(
+  [currentFocusedCellId, currentCellMap, currentCellOrder],
+  (focusedCellId, cellMap, cellOrder) => {
+    const index = cellOrder.indexOf(focusedCellId);
+    return cellOrder
+      .skip(index)
+      .filter(id => cellMap.getIn([id, "cell_type"]) === "code");
+  }
+);
+
+export const currentHiddenCellIds = createSelector(
+  [currentCellMap, currentCellOrder],
+  (cellMap, cellOrder) =>
+    cellOrder.filter(id => cellMap.getIn([id, "metadata", "inputHidden"]))
+);
+
+export const currentFilename = createSelector(
+  (state: AppState) => state.document.get("filename"),
+  identity
 );
