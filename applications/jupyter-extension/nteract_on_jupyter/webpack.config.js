f2eb6e754fc130bf8efdf0ef722f20b7fd711803
Kyle Kelley <rgbkrk@gmail.com>
2018-02-10 16:19:48 -0800

    follow rx pipeable operators webpack guidance

--- applications/jupyter-extension/nteract_on_jupyter/webpack.config.js
+++ applications/jupyter-extension/nteract_on_jupyter/webpack.config.js
@@ -2,7 +2,25 @@ const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 const webpack = require("webpack");
 const path = require("path");
 
+const nodeEnv = process.env.NODE_ENV || "development";
+const isProd = nodeEnv === "production";
+
+const rxPathMapping = require("rxjs/_esm5/path-mapping");
+
+const rxAliases = rxPathMapping();
+const aliases = {
+  "@nteract/transform-vdom": "@nteract/transform-vdom/src",
+  "@nteract/transforms": "@nteract/transforms/src",
+  "@nteract/markdown": "@nteract/markdown/src",
+  "@nteract/mathjax": "@nteract/mathjax/src",
+  "@nteract/core": "@nteract/core/src",
+  "rx-jupyter": "rx-jupyter/src",
+  "rx-binder": "rx-binder/src",
+  ...rxAliases
+};
+
 module.exports = {
+  devtool: isProd ? "hidden-source-map" : "cheap-eval-source-map",
   entry: {
     app: "./app/index.js",
     vendor: [
@@ -34,27 +52,38 @@ module.exports = {
   resolve: {
     mainFields: ["nteractDesktop", "module", "main"],
     extensions: [".js", ".jsx"],
-    alias: {
-      "@nteract/transform-vdom": "@nteract/transform-vdom/src",
-      "@nteract/transforms": "@nteract/transforms/src",
-      "@nteract/markdown": "@nteract/markdown/src",
-      "@nteract/mathjax": "@nteract/mathjax/src",
-      "@nteract/core": "@nteract/core/src",
-      "rx-jupyter": "rx-jupyter/src",
-      "rx-binder": "rx-binder/src"
-    }
+    alias: aliases
   },
   plugins: [
-    new LodashModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(nodeEnv)
+      }
+    }),
+    new webpack.HashedModuleIdsPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
 
-    // new webpack.IgnorePlugin(/\.(css|less)$/),
+    new webpack.IgnorePlugin(/\.(css|less)$/),
 
     // build vendor bundle (including common code chunks used in other bundles)
     new webpack.optimize.CommonsChunkPlugin({
       name: "vendor",
+      minChunks: Infinity,
       filename: "vendor.js"
     }),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: false,
+      compress: {
+        warnings: false,
+        pure_getters: true,
+        passes: 3,
+        screw_ie8: true,
+        sequences: false
+      },
+      output: { comments: false, beautify: true },
+      sourceMap: false
+    }),
+
     new webpack.SourceMapDevToolPlugin({
       filename: "[name].js.map",
       exclude: ["vendor.js"]
