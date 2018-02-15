5f8977299d28ea031049b8573405888dd9bd94dc
Kyle Kelley <rgbkrk@gmail.com>
2018-02-15 08:59:25 -0800

    create a readdirObservable

--- packages/fs-observable/src/index.js
+++ packages/fs-observable/src/index.js
@@ -36,3 +36,45 @@ export const readFileObservable = bindNodeCallback(fs.readFile);
 export const writeFileObservable = bindNodeCallback(fs.writeFile);
 
 export const mkdirpObservable = bindNodeCallback(mkdirp);
+
+type readdirCallback<T> = (err: ?ErrnoError, files: Array<T>) => void;
+
+function createReaddirCallback<T>(
+  observer: rxjs$Observer<Array<T>>
+): readdirCallback<T> {
+  return (err: ?ErrnoError, files: Array<T>) => {
+    if (err) {
+      observer.error(err);
+    } else {
+      observer.next(files);
+      observer.complete();
+    }
+  };
+}
+
+export const readdirObservable = (
+  path: string,
+  options?: string | { encoding: string }
+) =>
+  // TODO: readdir can resolve an Array<buffer>
+  // PR in progress: https://github.com/facebook/flow/pull/5820
+  // We'll default to string for now
+  Observable.create((observer: rxjs$Observer<Array<string>>) => {
+    if (!options) {
+      const callback: readdirCallback<string> = createReaddirCallback(observer);
+      fs.readdir(path, callback);
+    } else {
+      /*
+      // TODO: See above about readdir having the ability to resolve Array<buffer>
+      if (options === "buffer" || options.encoding === "buffer") {
+        const callback: readdirCallback<Buffer> = createReaddirCallback(
+          observer
+        );
+        fs.readdir(path, options, callback);
+        return;
+      }
+      */
+      const callback: readdirCallback<string> = createReaddirCallback(observer);
+      fs.readdir(path, options, callback);
+    }
+  });
