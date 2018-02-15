3db004c4cce7327170ee58860291348555e00ba7
Kyle Kelley <rgbkrk@gmail.com>
2018-02-15 11:31:28 -0800

    improve style of old fs-observable tests

--- packages/fs-observable/__tests__/fs-spec.js
+++ packages/fs-observable/__tests__/fs-spec.js
@@ -9,27 +9,32 @@ describe("unlinkObservable", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-  it("errors on unlink issue", () => {
+  it("errors on unlink issue", async function(done) {
     expect.assertions(3);
+    // File exists so we try to unlink it
     fs.existsSync.mockImplementation(() => true);
-    fs.unlink.mockImplementation((path, error) => error({ message: "lol" }));
-    unlinkObservable("path").subscribe(
-      () => {},
-      err => {
-        expect(err.message).toBe("lol");
-      },
-      () => done.fail()
+    // Unlink fails for some reason (permissions, etc.)
+    fs.unlink.mockImplementation((path, callback) =>
+      callback({ message: "forced failure" })
     );
+    try {
+      await unlinkObservable("path").toPromise();
+      // Should not pass through
+      done.fail();
+    } catch (err) {
+      expect(err.message).toBe("forced failure");
+    }
     expect(fs.existsSync).toBeCalledWith("path");
     expect(fs.unlink).toBeCalled();
+    done();
   });
-  it("completes and calls fs.existsSync, fs.unlink", () => {
+  it("completes and calls fs.existsSync, fs.unlink", async function() {
     expect.assertions(2);
     fs.existsSync.mockImplementation(() => true);
-    fs.unlink.mockImplementation(() => true);
-    unlinkObservable("path2")
-      .pipe(toArray())
-      .subscribe(() => {}, err => done.fail(err), () => {});
+    fs.unlink.mockImplementation((path, callback) => callback());
+
+    await unlinkObservable("path2").toPromise();
+
     expect(fs.existsSync).toBeCalledWith("path2");
     expect(fs.unlink).toBeCalled();
   });
