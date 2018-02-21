d814f5cb6791fc7d16dca56e88db55ae749c9bca
Andrew Seier <andseier@gmail.com>
2018-02-20 18:54:22 -0800

    Add core state types / functions for refs and ids.

---
+++ packages/core/src/types/state/ids.js
@@ -0,0 +1,14 @@
+// @flow
+
+// The *Id type is for external resources like the trailing id in:
+//   /api/kernels/9092-7679-9978-8822
+// ... where 9092-7679-9978-8822 is the KernelId.
+//
+// We call it "castTo*" to make it clear that you're not getting anything unique
+// out of the function.
+
+export opaque type HostId = string;
+export opaque type KernelId = string;
+
+export const castToHostId = (id: string): HostId => id;
+export const castToKernelId = (id: string): KernelId => id;
---
+++ packages/core/src/types/state/refs.js
@@ -0,0 +1,19 @@
+// @flow
+import uuid from "uuid";
+
+// TODO: this is a little odd that we have code inside a `/types` directory. The
+// reason is to allow strict typing for these ref functions.
+
+// Note that within this file, these can all be used as `string`.
+// However, _outside_ this file, they are no longer `string`, but actually the
+// opaque types that we've set.
+// See https://flow.org/en/docs/types/opaque-types/#toc-within-the-defining-file
+export opaque type HostRef = string;
+export opaque type KernelRef = string;
+export opaque type KernelspecsRef = string;
+export opaque type ContentRef = string;
+
+export const createHostRef = (): HostRef => uuid.v4();
+export const createKernelRef = (): KernelRef => uuid.v4();
+export const createKernelspecsRef = (): KernelspecsRef => uuid.v4();
+export const createContentRef = (): ContentRef => uuid.v4();
