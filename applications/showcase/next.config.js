3f0bc8710152b800cce5445deeda7ba08491a71f
Kyle Kelley <rgbkrk@gmail.com>
2018-03-02 07:09:16 -0800

    configure nteract showcase with our webpack config

--- applications/showcase/next.config.js
+++ applications/showcase/next.config.js
@@ -1,13 +1,16 @@
+const configurator = require("@nteract/webpack-configurator");
+
 module.exports = {
   webpack: (config, { buildId, dev }) => {
     config.module.rules.push({
       test: /\.js$/,
-      exclude: /node_modules/,
+      exclude: configurator.exclude,
       loader: "babel-loader"
     });
 
     config.resolve = Object.assign({}, config.resolve, {
-      mainFields: ["nteractDesktop", "es2015", "jsnext:main", "module", "main"]
+      mainFields: ["nteractDesktop", "es2015", "jsnext:main", "module", "main"],
+      alias: configurator.mergeDefaultAliases(config.resolve.alias)
     });
     return config;
   }
