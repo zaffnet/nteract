3239aea69bf4b2be1d2e4504e8798b40a0dee233
Andrew Seier <andseier@gmail.com>
2018-03-30 11:12:04 -0700

    Spruce up the directory listing.

--- packages/core/src/components/new-notebook-navigation/index.js
+++ packages/core/src/components/new-notebook-navigation/index.js
@@ -1,5 +1,9 @@
 // @flow
 
+/* eslint jsx-a11y/no-static-element-interactions: 0 */
+/* eslint jsx-a11y/click-events-have-key-events: 0 */
+/* eslint jsx-a11y/no-noninteractive-tabindex: 0 */
+
 /**
  * Plan:
  *
