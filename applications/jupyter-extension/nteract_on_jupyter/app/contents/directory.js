fe8e896c7748203d6d13655ce769c26cb7d5df08
Andrew Seier <andseier@gmail.com>
2018-03-30 15:35:23 -0700

    Keep directory items on one line.

--- applications/jupyter-extension/nteract_on_jupyter/app/contents/directory.js
+++ applications/jupyter-extension/nteract_on_jupyter/app/contents/directory.js
@@ -174,12 +174,7 @@ export class Directory extends React.PureComponent<DirectoryProps, *> {
           ul {
             list-style-type: none;
             padding: 0;
-            display: flex;
-            flex-direction: column;
-            flex-wrap: wrap;
-            height: ${40 *
-              ((this.props.content.model.items.size + (atRoot ? 0 : 1)) / 2)}px;
-            min-height: ${40 * 6}px;
+            margin: 0;
           }
         `}</style>
       </ul>
