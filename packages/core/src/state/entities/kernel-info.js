7109114930e08d1875ac914685a79a43d08bd0a8
Kyle Kelley <rgbkrk@gmail.com>
2018-03-31 09:05:31 -0700

    instantiate record before using in makeXRecord

--- packages/core/src/state/entities/kernel-info.js
+++ packages/core/src/state/entities/kernel-info.js
@@ -94,9 +94,27 @@ export type KernelInfo = {
 
 export type KernelInfoRecord = Immutable.RecordOf<KernelInfo>;
 
+// NOTE: This is not exported as the "makeXRecord" for KernelInfo so that we
+//       don't have nullables and can rely on the proper types. The developer is
+//       required to pass a fully instantiated KernelInfo into
+//       makeKernelInfoRecord below.
+const kernelInfoRecordMaker: Immutable.RecordFactory<
+  KernelInfo
+> = Immutable.Record({
+  protocolVersion: "0.0.0", // E.g. 1.2.3
+  implementation: "",
+  implementationVersion: "",
+  banner: "",
+  helpLinks: Immutable.List(),
+  languageName: "",
+  languageVersion: "",
+  mimetype: "",
+  fileExtension: "",
+  pygmentsLexer: "",
+  codemirrorMode: "text/plain",
+  nbconvertExporter: ""
+});
+
 export function makeKernelInfoRecord(kernelInfo: KernelInfo): KernelInfoRecord {
-  const maker: Immutable.RecordFactory<KernelInfo> = Immutable.Record(
-    kernelInfo
-  );
-  return maker();
+  return kernelInfoRecordMaker(kernelInfo);
 }
