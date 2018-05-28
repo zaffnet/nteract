e8ba72c249e041378cb2f12ec05bbc556454b25a
Kyle Kelley <rgbkrk@gmail.com>
2018-05-28 10:59:41 -0700

    pin vega things

--- applications/desktop/webpack.common.js
+++ applications/desktop/webpack.common.js
@@ -59,6 +59,7 @@ const rendererConfig = {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
         use: [
