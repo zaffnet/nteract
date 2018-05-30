3685283ef4bbff5121146798c16be605ea2a0e2d
Kyle Kelley <rgbkrk@gmail.com>
2018-05-30 08:44:43 -0700

    Make <PromptBuffer> a special case of <Prompt>

--- packages/presentational-components/src/components/prompt.js
+++ packages/presentational-components/src/components/prompt.js
@@ -1,27 +1,10 @@
 // @flow
-
 import * as React from "react";
-import css from "styled-jsx/css";
 
-const promptStyle = css`
-  .prompt {
-    font-family: monospace;
-    font-size: 12px;
-    line-height: 22px;
-
-    width: var(--prompt-width, 50px);
-    padding: 9px 0;
-
-    text-align: center;
-
-    color: var(--theme-cell-prompt-fg, black);
-    background-color: var(--theme-cell-prompt-bg, #fafafa);
-  }
-`;
-
-// Totally fake component for consistency with indents of the editor area
-
-export function promptText(props: PromptProps) {
+/**
+ * Generate what text goes inside the prompt based on the props to the prompt
+ */
+export function promptText(props: PromptProps): string {
   if (props.running) {
     return "[*]";
   }
@@ -37,33 +20,42 @@ export function promptText(props: PromptProps) {
 type PromptProps = {
   counter: number | null,
   running: boolean,
-  queued: boolean
+  queued: boolean,
+  blank: boolean
 };
 
 export class Prompt extends React.Component<PromptProps, null> {
   static defaultProps = {
     counter: null,
     running: false,
-    queued: false
+    queued: false,
+    blank: false
   };
 
   render() {
     return (
       <React.Fragment>
-        <div className="prompt">{promptText(this.props)}</div>
-        <style jsx>{promptStyle}</style>
+        <div className="prompt">
+          {this.props.blank ? null : promptText(this.props)}
+        </div>
+        <style jsx>{`
+          .prompt {
+            font-family: monospace;
+            font-size: 12px;
+            line-height: 22px;
+
+            width: var(--prompt-width, 50px);
+            padding: 9px 0;
+
+            text-align: center;
+
+            color: var(--theme-cell-prompt-fg, black);
+            background-color: var(--theme-cell-prompt-bg, #fafafa);
+          }
+        `}</style>
       </React.Fragment>
     );
   }
 }
 
-export class PromptBuffer extends React.Component<any, null> {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="prompt" />
-        <style jsx>{promptStyle}</style>
-      </React.Fragment>
-    );
-  }
-}
+export const PromptBuffer = () => <Prompt blank />;
