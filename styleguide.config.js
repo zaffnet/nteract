bde059e065e36c5f1492d0fca92f161efc4d8d36
Kyle Kelley <rgbkrk@gmail.com>
2018-05-31 00:02:13 -0700

    override the Logo section for our component page

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,9 +1,21 @@
+// @format
+const path = require("path");
+
 module.exports = {
+  title: "nteract components",
   components: "packages/presentational-components/src/components/*.js",
+  // For overriding the components styleguidist uses
+  styleguideComponents: {
+    LogoRenderer: path.join(
+      __dirname,
+      "packages",
+      "styleguide-components",
+      "logo.js"
+    )
+  },
   webpackConfig: {
     module: {
       rules: [
-        // Babel loader, will use your project’s .babelrc
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
