0adf2baeb539a69c1c68ad4bbcfa5ad1f9fc08c0
Kyle Kelley <rgbkrk@gmail.com>
2018-07-28 15:01:35 -0700

    export kernel for host-cache (#3128)
    Missing an export

--- packages/host-cache/src/index.js
+++ packages/host-cache/src/index.js
@@ -3,4 +3,4 @@ import Kernel from "./components/kernel.js";
 import Host from "./components/host.js";
 import { LocalHostStorage } from "./host-storage.js";
 
-export { Host, LocalHostStorage };
+export { Host, LocalHostStorage, Kernel };
