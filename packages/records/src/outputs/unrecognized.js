87067ce9c9ed4e2ca7f45fc155c17110178f245b
Ben Russert <benrussert@gmail.com>
2018-07-31 15:45:42 -0600

    Fix output types

--- packages/records/src/outputs/unrecognized.js
+++ packages/records/src/outputs/unrecognized.js
@@ -3,42 +3,22 @@
 // Someday in the future, the notebook format or the message spec might get a new output type. We need to make sure that our
 // frontends are able to have a known fallback when an output type isn't known.
 
-import * as Immutable from "immutable";
-
 // In-memory version
-type UnrecognizedOutput = {
-  outputType: string,
-  /* The raw output, for writing back to disk properly */
-  raw: Immutable.Map<any, any>
-};
+export type UnrecognizedOutput = any;
 
 // On disk
 export type NbformatUnrecognizedOutput = {
   output_type: string // Technically, not one of "execute_result", "display_data", "stream", or "error"
 };
 
-export type UnrecognizedOutputRecord = Immutable.RecordOf<UnrecognizedOutput>;
+export function makeUnrecognizedOutputRecord(): UnrecognizedOutput {
+  const defaultUnrecognizedOutput = {
+    outputType: "unrecognized"
+  };
 
-const unrecognizedOutputRecordMaker: Immutable.RecordFactory<
-  UnrecognizedOutput
-> = Immutable.Record({
-  outputType: "unrecognized",
-  // This must get overridden below
-  raw: Immutable.Map()
-});
-
-export function makeUnrecognizedOutputRecord(
-  unrecognizedOutput: UnrecognizedOutput
-): UnrecognizedOutputRecord {
-  return unrecognizedOutputRecordMaker(unrecognizedOutput);
+  return defaultUnrecognizedOutput;
 }
 
-export function unrecognizedRecordFromNbformat(
-  s: NbformatUnrecognizedOutput
-): UnrecognizedOutputRecord {
-  return makeUnrecognizedOutputRecord({
-    outputType: s.output_type,
-    // ⚠️ This has the potential of being mutated within since we're only turning it into a top level Immutable.Map
-    raw: Immutable.Map(s)
-  });
+export function unrecognizedRecordFromNbformat(): UnrecognizedOutput {
+  return makeUnrecognizedOutputRecord();
 }
