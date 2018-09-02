4338d32d4dab4d18a477aeb5753624fac55d67b2
Kyle Kelley <rgbkrk@gmail.com>
2018-09-02 09:33:47 -0700

    remove unused <MathJax.Text />

--- packages/mathjax/src/index.js
+++ packages/mathjax/src/index.js
@@ -2,7 +2,6 @@
 
 import Node from "./node";
 import Context from "./context";
-import Text from "./text";
 
-export { Node, Context, Text };
-export default { Node, Context, Text };
+export { Node, Context };
+export default { Node, Context };
