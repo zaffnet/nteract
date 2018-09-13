83bdec7d7c3c114cb8ac8cec000569c81a9ce680
Elijah Meeks <elijahmeeks@gmail.com>
2018-09-12 17:20:38 -0700

    cleanup
    Remove console log
    Honor height

--- packages/transform-dataresource/src/charts/grid.js
+++ packages/transform-dataresource/src/charts/grid.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import VirtualizedGrid from "../virtualized-grid";
 import ReactTable from "react-table";
 import ReactTableStyles from "../css/react-table";
 import withFixedColumns from "react-table-hoc-fixed-columns";
@@ -7,25 +6,21 @@ const ReactTableFixedColumns = withFixedColumns(ReactTable);
 
 export const DataResourceTransformGrid = ({
   data: { data, schema },
-  theme,
-  expanded,
   height
 }) => {
-  console.log("data, schema", data, schema, theme, height, expanded);
   const tableColumns = schema.fields.map((f, i) => ({
     Header: f.name,
     accessor: f.name,
     fixed: i === 0 && "left"
   }));
 
-  console.log("theme", theme);
   return (
     <div style={{ width: "calc(100vw - 150px)" }}>
       <ReactTableFixedColumns
         data={data}
         columns={tableColumns}
         style={{
-          height: "400px"
+          height: `${height}px`
         }}
         className="-striped -highlight"
       />
@@ -33,15 +28,5 @@ export const DataResourceTransformGrid = ({
         {ReactTableStyles}
       </style>
     </div>
-  );
-  return (
-    <VirtualizedGrid
-      data={data}
-      schema={schema}
-      theme={theme}
-      expanded={expanded}
-      height={height}
-      // style={{ marginRight: "10px" }}
-    />
   );
 };
