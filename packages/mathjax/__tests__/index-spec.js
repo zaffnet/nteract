d4361e1a81d1133c57df6d7fe7f5426defe1079a
Lukas Geiger <lukas.geiger94@gmail.com>
2018-10-10 00:35:05 +0200

    Check that load script function is called

--- packages/mathjax/__tests__/index-spec.js
+++ packages/mathjax/__tests__/index-spec.js
@@ -1,22 +1,31 @@
 import * as React from "react";
-import { Node, Provider } from "../src";
-
+import Enzyme, { mount } from "enzyme";
 import toJson from "enzyme-to-json";
 
-import Enzyme, { mount } from "enzyme";
+jest.mock("../src/load-script");
+import loadScript from "../src/load-script";
+import { Node, Provider } from "../src";
 
 describe("MathJax", () => {
   test("Node can be renderered without provider", () => {
     const wrapper = mount(<Node>x^2 + y = 3</Node>);
+    expect(loadScript).toHaveBeenLastCalledWith(
+      "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML",
+      expect.any(Function)
+    );
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   test("Nodes use existing provider", () => {
     const wrapper = mount(
-      <Provider>
+      <Provider src="script-src">
         <Node>x^2 + y = 3</Node>
         <Node>x^3 + y = 2</Node>
       </Provider>
+    );
+    expect(loadScript).toHaveBeenLastCalledWith(
+      "script-src",
+      expect.any(Function)
     );
     expect(toJson(wrapper)).toMatchSnapshot();
   });
