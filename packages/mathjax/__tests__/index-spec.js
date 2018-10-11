88f332fa15a72c04bfb4cc9b943470839b15aed4
Lukas Geiger <lukas.geiger94@gmail.com>
2018-10-11 22:39:34 +0200

    Add simple snapshot test for Text node

--- packages/mathjax/__tests__/index-spec.js
+++ packages/mathjax/__tests__/index-spec.js
@@ -4,13 +4,16 @@ import toJson from "enzyme-to-json";
 
 jest.mock("../src/load-script");
 import loadScript from "../src/load-script";
-import { Node, Provider } from "../src";
+import { Node, Provider, Text } from "../src";
+
+const mathJaxUrl =
+  "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML";
 
 describe("MathJax", () => {
   test("Node can be renderered without provider", () => {
     const wrapper = mount(<Node>x^2 + y = 3</Node>);
     expect(loadScript).toHaveBeenLastCalledWith(
-      "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML",
+      mathJaxUrl,
       expect.any(Function)
     );
     expect(toJson(wrapper)).toMatchSnapshot();
@@ -25,6 +28,15 @@ describe("MathJax", () => {
     );
     expect(loadScript).toHaveBeenLastCalledWith(
       "script-src",
+      expect.any(Function)
+    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test("Text node renders LaTeX text", () => {
+    const wrapper = mount(<Text>This is a LaTeX equation $x^2 + y = 3$</Text>);
+    expect(loadScript).toHaveBeenLastCalledWith(
+      mathJaxUrl,
       expect.any(Function)
     );
     expect(toJson(wrapper)).toMatchSnapshot();
