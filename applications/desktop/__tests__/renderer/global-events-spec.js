zaffnet/nteract

commit f89e94c89ab20b08804564c55e78618a83bbdc01

Send kernel shutdown messages when closing windows.
This adds a closeNotebookEpic which orchestrates all aspects of closing
a notebook window, including prompting about unsaved changes and
shutting down kernels properly (using the shutdown msg before resorting
to a SIGKILL).

applications/desktop/src/main/index.js
@@ -36,7 +36,11 @@ import { loadFullMenu, loadTrayMenu } from "./menu";
 
 import prepareEnv from "./prepare-env";
 import initializeKernelSpecs from "./kernel-specs";
-import { setKernelSpecs } from "./actions";
+import { setKernelSpecs, setQuittingState } from "./actions";
+import {
+  QUITTING_STATE_NOT_STARTED,
+  QUITTING_STATE_QUITTING
+} from "./reducers.js";
 
 import configureStore from "./store";
 
@@ -93,6 +97,11 @@ ipc.on("new-kernel", (event, k) => {
 
 ipc.on("open-notebook", (event, filename) => {
   launch(resolve(filename));
+});
+
+ipc.on("show-message-box", (event, arg) => {
+  const response = dialog.showMessageBox(arg);
+  event.sender.send("show-message-box-response", response);
 });
 
 app.on("ready", initAutoUpdater);
@@ -187,16 +196,43 @@ electronReady$
   .pipe(takeUntil(appAndKernelSpecsReady))
   .subscribe(createSplashSubscriber());
 
-function closeAppOnNonDarwin() {
-  // On macOS, we want to keep the app and menu bar active
-  if (process.platform !== "darwin") {
+app.on("before-quit", e => {
+  // When notebook windows are open we need to orchestrate shutdown manually.
+  const windows = BrowserWindow.getAllWindows();
+  if (
+    windows.length > 0 &&
+    store.getState().get("quittingState") === QUITTING_STATE_NOT_STARTED
+  ) {
+    e.preventDefault();
+    store.dispatch(setQuittingState(QUITTING_STATE_QUITTING));
+
+    // Trigger each windows' closeNotebookEpic. If and when all windows are closed,
+    // the window-all-closed event will fire and we will complete the quit action.
+    windows.forEach(win => win.close());
+  }
+});
+
+const windowAllClosed = fromEvent(app, "window-all-closed");
+windowAllClosed.pipe(skipUntil(appAndKernelSpecsReady)).subscribe(() => {
+  // On macOS:
+  // - If user manually closed the last window, we want to keep the app and
+  //   menu bar active.
+  // - If the window was closed programmatically as part of a quit, and not
+  //   canceled during notebook shutdown, then we proceed w/ the quit.
+  // All other platforms:
+  // - Quit when last window closed.
+  if (
+    process.platform !== "darwin" ||
+    store.getState().get("quittingState") === QUITTING_STATE_QUITTING
+  ) {
     app.quit();
   }
-}
-const windowAllClosed = fromEvent(app, "window-all-closed");
-windowAllClosed
-  .pipe(skipUntil(appAndKernelSpecsReady))
-  .subscribe(closeAppOnNonDarwin);
+});
+
+ipc.on("close-notebook-canceled", (event, k) => {
+  // User canceled, so interpret that as cancelling any in-flight app-wide quit
+  store.dispatch(setQuittingState(QUITTING_STATE_NOT_STARTED));
+});
 
 const openFile$ = fromEvent(app, "open-file", (event, filename) => ({
   event,

applications/desktop/src/main/menu.js
@@ -1,12 +1,5 @@
 /* @flow strict */
-import {
-  dialog,
-  app,
-  shell,
-  Menu,
-  ipcMain as ipc,
-  BrowserWindow
-} from "electron";
+import { dialog, app, shell, Menu, BrowserWindow } from "electron";
 import * as path from "path";
 import { sortBy } from "lodash";
 import { launch, launchNewNotebook } from "./launch";

applications/desktop/src/notebook/actionTypes.js
@@ -1,0 +1,21 @@
+// @flow strict
+
+import type { ContentRef } from "@nteract/core";
+
+import type { DesktopNotebookClosingState } from "./state";
+
+export const CLOSE_NOTEBOOK = "DESKTOP/CLOSE_NOTEBOOK";
+export type CloseNotebook = {
+  type: "DESKTOP/CLOSE_NOTEBOOK",
+  payload: {
+    contentRef: ContentRef
+  }
+};
+
+export const CLOSE_NOTEBOOK_PROGRESS = "DESKTOP/CLOSE_NOTEBOOK_PROGRESS";
+export type CloseNotebookProgress = {
+  type: "DESKTOP/CLOSE_NOTEBOOK_PROGRESS",
+  payload: {
+    newState: DesktopNotebookClosingState
+  }
+};

applications/desktop/src/notebook/actions.js
@@ -1,0 +1,25 @@
+// @flow strict
+
+import * as actionTypes from "./actionTypes";
+
+import type { ContentRef } from "@nteract/core";
+
+import type { DesktopNotebookClosingState } from "./state";
+
+export function closeNotebook(payload: {
+  contentRef: ContentRef
+}): actionTypes.CloseNotebook {
+  return {
+    type: actionTypes.CLOSE_NOTEBOOK,
+    payload
+  };
+}
+
+export function closeNotebookProgress(payload: {
+  newState: DesktopNotebookClosingState
+}): actionTypes.CloseNotebookProgress {
+  return {
+    type: actionTypes.CLOSE_NOTEBOOK_PROGRESS,
+    payload
+  };
+}

applications/desktop/src/notebook/epics/close-notebook.js
@@ -1,0 +1,164 @@
+// Flow goes crazy when this file is typed, raising errors throughout the project.
+// I believe it's related to the intersection type of DesktopNotebookAppState.
+// Lots of open bugs around intersection types, and they're used inside Immutable.js too, so layers upon layers.
+// https://github.com/facebook/flow/issues?utf8=%E2%9C%93&q=is%3Aissue+is%3Aopen+intersect
+
+import * as actions from "../actions";
+import * as actionTypes from "../actionTypes";
+
+import {
+  makeDesktopNotebookRecord,
+  DESKTOP_NOTEBOOK_CLOSING_NOT_STARTED,
+  DESKTOP_NOTEBOOK_CLOSING_STARTED,
+  DESKTOP_NOTEBOOK_CLOSING_READY_TO_CLOSE
+} from "../state.js";
+
+import {
+  actionTypes as coreActionTypes,
+  actions as coreActions,
+  selectors
+} from "@nteract/core";
+
+import { Observable } from "rxjs/Observable";
+import { empty } from "rxjs/observable/empty";
+import { of } from "rxjs/observable/of";
+import { zip } from "rxjs/observable/zip";
+import { concat } from "rxjs/observable/concat";
+
+import {
+  catchError,
+  concatMap,
+  exhaustMap,
+  filter,
+  map,
+  switchMap,
+  take,
+  tap,
+  timeout
+} from "rxjs/operators";
+
+import { ActionsObservable, ofType } from "redux-observable";
+
+import { remote, ipcRenderer as ipc } from "electron";
+
+export const closeNotebookEpic = (action$: ActionsObservable<*>, store: *) =>
+  action$.pipe(
+    ofType(actionTypes.CLOSE_NOTEBOOK),
+    exhaustMap((action: actionTypes.CloseNotebook) => {
+      const contentRef = action.payload.contentRef;
+      const state = store.getState();
+      const model = selectors.model(state, { contentRef });
+
+      var dirtyPromptObservable: Observable<boolean>;
+      if (selectors.notebook.isDirty(model)) {
+        dirtyPromptObservable = Observable.create(observer => {
+          const promptDialog = {
+            type: "question",
+            buttons: ["Yes", "No"],
+            title: "Confirm",
+            message: "Unsaved data will be lost. Are you sure you want to quit?"
+          };
+          ipc.once("show-message-box-response", (event, arg) => {
+            observer.next(arg === 0);
+            observer.complete();
+          });
+          ipc.send("show-message-box", promptDialog);
+        });
+      } else {
+        dirtyPromptObservable = of(true);
+      }
+
+      const killKernelActions = [];
+      const killKernelAwaits = [];
+      state.core.entities.kernels.byRef.forEach((kernel, kernelRef) => {
+        // Skip if kernel unknown
+        if (!kernel || !kernel.type) {
+          return;
+        }
+
+        if (kernel.type === "zeromq") {
+          killKernelActions.push(
+            coreActions.killKernel({
+              restarting: false,
+              kernelRef: kernelRef
+            })
+          );
+
+          killKernelAwaits.push(
+            action$.pipe(
+              ofType(
+                coreActionTypes.KILL_KERNEL_SUCCESSFUL,
+                coreActionTypes.KILL_KERNEL_FAILED
+              ),
+              filter(action => action.payload.kernelRef === kernelRef),
+              take(1)
+            )
+          );
+        } else if (kernel.type === "websocket") {
+          console.log(
+            "Need to implement a way to shutdown websocket kernels on desktop"
+          );
+        }
+      });
+
+      const killKernels = of(...killKernelActions);
+
+      const awaitKillKernelResults = zip(...killKernelAwaits).pipe(
+        timeout(1000 * 5), // This should be at least as long as the timeout used to wait for kernel to reply to shutdown msgs
+        tap(result => {
+          for (let r of result) {
+            console.log(JSON.stringify(r)); // To see these in terminal, set ELECTRON_ENABLE_LOGGING=1. Could also start more explicitly routing them to main process stdout.
+          }
+        }),
+        concatMap(_ =>
+          // We don't need the results. Further, allowing the Array<Action> to flow through error middleware crashed
+          // node.js for me, with "Error: async hook stack has become corrupted (actual: 1528, expected: 0)".
+          // Seemed related to the error middleware not expecting an Array (returning result[0] avoids the crash as
+          // well).
+          empty()
+        ),
+        catchError((error: Error) => {
+          console.log(
+            "One or more kernels failed to shutdown properly in the allocated time."
+          );
+          console.log(error.message);
+          return empty(); // Just carry on with closing.
+        })
+      );
+
+      const updateClosingState = of(
+        actions.closeNotebookProgress({
+          newState: DESKTOP_NOTEBOOK_CLOSING_READY_TO_CLOSE // This is what allows the window to unload
+        })
+      );
+
+      const initiateClose = Observable.create(observer => {
+        console.log("Kernel shutdown complete; closing notebook window...");
+        window.close();
+        observer.complete();
+      });
+
+      return dirtyPromptObservable.pipe(
+        concatMap((proceed: boolean) => {
+          if (!proceed) {
+            // Cancel any full-app shutdown in flight
+            ipc.send("close-notebook-canceled");
+
+            // Reset notebook state to allow another attempt later
+            return of(
+              actions.closeNotebookProgress({
+                newState: DESKTOP_NOTEBOOK_CLOSING_NOT_STARTED
+              })
+            );
+          }
+
+          return concat(
+            killKernels,
+            awaitKillKernelResults,
+            updateClosingState,
+            initiateClose
+          );
+        })
+      );
+    })
+  );

applications/desktop/src/notebook/global-events.js
@@ -7,32 +7,21 @@ import { selectors } from "@nteract/core";
 
 import type { AppState, KernelRef, ContentRef } from "@nteract/core";
 
-import { killKernelImmediately } from "./epics/zeromq-kernels";
+import { ipcMain as ipc } from "electron";
 
-export function unload(store: Store<AppState, Action>) {
-  const state = store.getState();
+import * as actions from "./actions";
 
-  state.core.entities.kernels.byRef.forEach((kernel, kernelRef) => {
-    // Skip if kernel unknown
-    if (!kernel || !kernel.type) {
-      return;
-    }
+import type { DesktopNotebookAppState } from "./state.js";
+import {
+  DESKTOP_NOTEBOOK_CLOSING_NOT_STARTED,
+  DESKTOP_NOTEBOOK_CLOSING_READY_TO_CLOSE
+} from "./state.js";
 
-    if (kernel.type === "zeromq") {
-      try {
-        killKernelImmediately(kernel);
-      } catch (e) {
-        alert(`Trouble shutting down - ${e.message}`);
-      }
-    } else if (kernel.type === "websocket") {
-      alert("Need to implement a way to shutdown websocket kernels on desktop");
-    }
-  });
-}
+const urljoin = require("url-join");
 
 export function beforeUnload(
   contentRef: ContentRef,
-  store: Store<AppState, Action>,
+  store: Store<DesktopNotebookAppState, Action>,
   e: *
 ) {
   const state = store.getState();
@@ -43,16 +32,24 @@ export function beforeUnload(
     return;
   }
 
-  if (selectors.notebook.isDirty(model)) {
-    // Will prevent closing "will-prevent-unload"
-    e.returnValue = true;
+  const closingState = state.desktopNotebook.closingState;
+  if (closingState === DESKTOP_NOTEBOOK_CLOSING_NOT_STARTED) {
+    // Dispatch asynchronously since returning ASAP is imperative for canceling close/unload.
+    // See https://github.com/electron/electron/issues/12668
+    setTimeout(
+      () => store.dispatch(actions.closeNotebook({ contentRef: contentRef })),
+      0
+    );
+  }
+
+  if (closingState !== DESKTOP_NOTEBOOK_CLOSING_READY_TO_CLOSE) {
+    return false;
   }
 }
 
 export function initGlobalHandlers(
   contentRef: ContentRef,
-  store: Store<AppState, Action>
+  store: Store<DesktopNotebookAppState, Action>
 ) {
   window.onbeforeunload = beforeUnload.bind(null, contentRef, store);
-  window.onunload = unload.bind(null, store);
 }

applications/desktop/__tests__/renderer/global-events-spec.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 import * as globalEvents from "../../src/notebook/global-events";
 import * as Immutable from "immutable";
 
@@ -10,7 +12,20 @@ import {
   makeDocumentRecord
 } from "@nteract/core";
 
-const createStore = (contentRef, content) => ({
+import {
+  makeDesktopNotebookRecord,
+  DESKTOP_NOTEBOOK_CLOSING_NOT_STARTED,
+  DESKTOP_NOTEBOOK_CLOSING_STARTED,
+  DESKTOP_NOTEBOOK_CLOSING_READY_TO_CLOSE
+} from "../../src/notebook/state.js";
+
+import * as actions from "../../src/notebook/actions.js";
+
+const createStore = (
+  contentRef,
+  content,
+  closingState: DesktopNotebookClosingState
+) => ({
   getState: () => ({
     core: makeStateRecord({
       entities: makeEntitiesRecord({
@@ -21,12 +36,16 @@ const createStore = (contentRef, content) => ({
           })
         })
       })
-    })
+    }),
+    desktopNotebook: makeDesktopNotebookRecord().set(
+      "closingState",
+      closingState
+    )
   })
 });
 
 describe("beforeUnload", () => {
-  test("should set event.returnValue if notebook is modified to prevent unload", () => {
+  test("if we are not yet closing the notebook, should initiate closeNotebook and cancel close event", done => {
     const contentRef = createContentRef();
     const store = createStore(
       contentRef,
@@ -35,38 +54,58 @@ describe("beforeUnload", () => {
           notebook: "not same",
           savedNotebook: "different"
         })
-      })
+      }),
+      DESKTOP_NOTEBOOK_CLOSING_NOT_STARTED
     );
+
+    store.dispatch = action => {
+      expect(action).toEqual(actions.closeNotebook({ contentRef: contentRef }));
+      done();
+    };
 
     const event = {};
 
-    globalEvents.beforeUnload(contentRef, store, event);
-
-    expect(event.returnValue).toBeDefined();
+    const result = globalEvents.beforeUnload(contentRef, store, event);
+    expect(result).toBe(false);
   });
 
-  test("should should not set event.returnValue if notebook is saved", () => {
+  test("if we are in the process of closing the notebook, should continue to cancel close event", () => {
     const contentRef = createContentRef();
     const store = createStore(
       contentRef,
       makeNotebookContentRecord({
         model: makeDocumentRecord({
-          notebook: "same",
-          savedNotebook: "same"
+          notebook: "not same",
+          savedNotebook: "different"
         })
-      })
+      }),
+      DESKTOP_NOTEBOOK_CLOSING_STARTED
     );
-
     const event = {};
+    const result = globalEvents.beforeUnload(contentRef, store, event);
+    expect(result).toBe(false);
+  });
 
-    globalEvents.beforeUnload(contentRef, store, event);
-
-    expect(event.returnValue).toBeUndefined();
+  test("if we have completed closing the notebook, should not cancel close event", () => {
+    const contentRef = createContentRef();
+    const store = createStore(
+      contentRef,
+      makeNotebookContentRecord({
+        model: makeDocumentRecord({
+          notebook: "not same",
+          savedNotebook: "different"
+        })
+      }),
+      DESKTOP_NOTEBOOK_CLOSING_READY_TO_CLOSE
+    );
+    const event = {};
+    const result = globalEvents.beforeUnload(contentRef, store, event);
+    expect(result).toBeUndefined();
   });
 });
 
 describe("initGlobalHandlers", () => {
-  test("adds an unload poperty to the window object", () => {
+  test("adds an unload property to the window object", () => {
     const contentRef = createContentRef();
     const store = createStore(contentRef);
 

applications/desktop/src/main/actions.js
@@ -1,8 +1,17 @@
 // @flow strict
 
+import type { QuittingState } from "./reducers.js";
+
 export function setKernelSpecs(kernelSpecs: KernelSpecs) {
   return {
     type: "SET_KERNELSPECS",
     kernelSpecs: kernelSpecs
+  };
+}
+
+export function setQuittingState(newState: QuittingState) {
+  return {
+    type: "SET_QUITTING_STATE",
+    newState: newState
   };
 }

commit caf2863d71680492735950b2537efb5a582f4f50

Fixes desktop reload feature, which I broke in f89e94c89.
The closeNotebookEpic initiated in the renderer's onbeforeunload
was being fired for reload as well, and was hard-coded to finish
the workflow by asking the main process to close the window.

The simplest way I found to distinguish between a close and
a reload was to wire a custom "reload" message, to be sent
from the Reload menu/shortcut in the main process to the
renderer. This controls how the closeNotebookEpic will
finish up, messaging back to the main process asking it
to either close or reload the window.

I considered renaming the whole epic to couple it less
tightly to close and acknowledge that it handles reload,
but there are a lot of types/constants around and it
doesn't seem worth the effort.

A failed attempt, recorded for posterity:
I initially tried to distinguish between close and reload
cases purely within the renderer. By some readings
of the docs the close event should fire only on closes,
and the beforeunload even should fire in both close and
reload, making it possible to distinguish between the two.
Unfortunately it seems that close events wired from within
the renderer do not actually work.

applications/desktop/src/main/index.js
@@ -99,6 +99,14 @@ ipc.on("open-notebook", (event, filename) => {
   launch(resolve(filename));
 });
 
+ipc.on("reload", (event) => { // Based on https://github.com/electron/electron/blob/b50f86ef43b17f90c295349d8ca93751ad9045a6/lib/browser/rpc-server.js#L411-L418
+  const window = event.sender.getOwnerBrowserWindow();
+  if (window) {
+    window.reload();
+  }
+  event.returnValue = null
+});
+
 ipc.on("show-message-box", (event, arg) => {
   const response = dialog.showMessageBox(arg);
   event.sender.send("show-message-box-response", response);

applications/desktop/src/main/menu.js
@@ -1,5 +1,5 @@
 /* @flow strict */
-import { dialog, app, shell, Menu, BrowserWindow } from "electron";
+import { dialog, app, shell, Menu, BrowserWindow, ipcRenderer as ipc } from "electron";
 import * as path from "path";
 import { sortBy } from "lodash";
 import { launch, launchNewNotebook } from "./launch";
@@ -461,11 +461,7 @@ export function loadFullMenu(store: * = global.store) {
         label: "Reload",
         enabled: BrowserWindow.getAllWindows().length > 0,
         accelerator: "CmdOrCtrl+R",
-        click: (item, focusedWindow) => {
-          if (focusedWindow) {
-            focusedWindow.reload();
-          }
-        }
+        click: createSender("reload")
       },
       {
         label: "Toggle Full Screen",

applications/desktop/src/notebook/actionTypes.js
@@ -8,7 +8,8 @@ export const CLOSE_NOTEBOOK = "DESKTOP/CLOSE_NOTEBOOK";
 export type CloseNotebook = {
   type: "DESKTOP/CLOSE_NOTEBOOK",
   payload: {
-    contentRef: ContentRef
+    contentRef: ContentRef,
+    reloading: boolean
   }
 };
 

applications/desktop/src/notebook/actions.js
@@ -7,7 +7,8 @@ import type { ContentRef } from "@nteract/core";
 import type { DesktopNotebookClosingState } from "./state";
 
 export function closeNotebook(payload: {
-  contentRef: ContentRef
+  contentRef: ContentRef,
+  reloading: boolean
 }): actionTypes.CloseNotebook {
   return {
     type: actionTypes.CLOSE_NOTEBOOK,

applications/desktop/src/notebook/epics/close-notebook.js
@@ -129,8 +129,14 @@ export const closeNotebookEpic = (action$: ActionsObservable<*>, store: *) =>
       );
 
       const initiateClose = Observable.create(observer => {
-        console.log("Kernel shutdown complete; closing notebook window...");
-        window.close();
+        if (action.payload.reloading) {
+          console.log("Kernel shutdown complete; reloading notebook window...");
+          ipc.send("reload");
+        } else {
+          console.log("Kernel shutdown complete; closing notebook window...");
+          window.close();
+        }
+
         observer.complete();
       });
 

applications/desktop/src/notebook/global-events.js
@@ -2,6 +2,8 @@
 
 import type { Store } from "redux";
 
+import { ipcRenderer as ipc } from "electron";
+
 import { selectors } from "@nteract/core";
 
 import type { ContentRef } from "@nteract/core";
@@ -14,9 +16,10 @@ import {
   DESKTOP_NOTEBOOK_CLOSING_READY_TO_CLOSE
 } from "./state.js";
 
-export function beforeUnload(
+export function onBeforeUnloadOrReload(
   contentRef: ContentRef,
-  store: Store<DesktopNotebookAppState, Action>
+  store: Store<DesktopNotebookAppState, Action>,
+  reloading: boolean
 ) {
   const state = store.getState();
   const model = selectors.model(state, { contentRef });
@@ -31,7 +34,7 @@ export function beforeUnload(
     // Dispatch asynchronously since returning ASAP is imperative for canceling close/unload.
     // See https://github.com/electron/electron/issues/12668
     setTimeout(
-      () => store.dispatch(actions.closeNotebook({ contentRef: contentRef })),
+      () => store.dispatch(actions.closeNotebook({ contentRef: contentRef, reloading })),
       0
     );
   }
@@ -45,5 +48,15 @@ export function initGlobalHandlers(
   contentRef: ContentRef,
   store: Store<DesktopNotebookAppState, Action>
 ) {
-  window.onbeforeunload = beforeUnload.bind(null, contentRef, store);
+  // This wiring of onBeforeUnloadOrReload is meant to handle:
+  // - User closing window by hand
+  // - Programmatic close from main process such as during a quit
+  window.onbeforeunload = onBeforeUnloadOrReload.bind(null, contentRef, store, false);
+
+  // This is our manually orchestrated reload. Tried using onclose vs. onbeforeunload
+  // to distinguish between the close and reload cases, but onclose doesn't fire
+  // reliably when wired from inside the renderer.
+  // In our manually-orchestrated reload, onbeforeunload will still fire
+  // at the end, but by then we'd transitioned our closingState such that it's a no-op.
+  ipc.on("reload", () => onBeforeUnloadOrReload(contentRef, store, true));
 }

applications/desktop/__tests__/renderer/global-events-spec.js
@@ -1,5 +1,7 @@
 /* @flow */
 
+import { ipcRenderer as ipc } from "../../__mocks__/electron";
+
 import * as globalEvents from "../../src/notebook/global-events";
 import * as Immutable from "immutable";
 
@@ -44,7 +46,7 @@ const createStore = (
   })
 });
 
-describe("beforeUnload", () => {
+describe("onBeforeUnloadOrReload", () => {
   test("if we are not yet closing the notebook, should initiate closeNotebook and cancel close event", done => {
     const contentRef = createContentRef();
     const store = createStore(
@@ -59,13 +61,13 @@ describe("beforeUnload", () => {
     );
 
     store.dispatch = action => {
-      expect(action).toEqual(actions.closeNotebook({ contentRef: contentRef }));
+      expect(action).toEqual(actions.closeNotebook({ contentRef: contentRef, reloading: false }));
       done();
     };
 
     const event = {};
 
-    const result = globalEvents.beforeUnload(contentRef, store, event);
+    const result = globalEvents.onBeforeUnloadOrReload(contentRef, store, false, event);
     expect(result).toBe(false);
   });
 
@@ -82,7 +84,7 @@ describe("beforeUnload", () => {
       DESKTOP_NOTEBOOK_CLOSING_STARTED
     );
     const event = {};
-    const result = globalEvents.beforeUnload(contentRef, store, event);
+    const result = globalEvents.onBeforeUnloadOrReload(contentRef, store, event, false);
     expect(result).toBe(false);
   });
 
@@ -99,7 +101,7 @@ describe("beforeUnload", () => {
       DESKTOP_NOTEBOOK_CLOSING_READY_TO_CLOSE
     );
     const event = {};
-    const result = globalEvents.beforeUnload(contentRef, store, event);
+    const result = globalEvents.onBeforeUnloadOrReload(contentRef, store, event, false);
     expect(result).toBeUndefined();
   });
 });
@@ -110,7 +112,18 @@ describe("initGlobalHandlers", () => {
     const store = createStore(contentRef);
 
     globalEvents.initGlobalHandlers(contentRef, store);
-    expect(global.window.onunload).toBeDefined();
+
     expect(global.window.onbeforeunload).toBeDefined();
+  });
+
+  test("wires a listener for a reload msg from main process", (done) => {
+    const contentRef = createContentRef();
+    const store = createStore(contentRef);
+
+    ipc.on = (event, callback) => {
+      if (event == "reload") done();
+    };
+
+    globalEvents.initGlobalHandlers(contentRef, store);
   });
 });