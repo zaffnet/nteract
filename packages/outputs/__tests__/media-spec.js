bc2bf95f7588bab95f57c9437c0e910119ee316c
Ben Russert <benrussert@gmail.com>
2018-10-12 19:54:14 -0600

    Add Media.JSON component

--- packages/outputs/__tests__/media-spec.js
+++ packages/outputs/__tests__/media-spec.js
@@ -1,6 +1,8 @@
-import React from "react";
+import * as React from "react";
 import { mount } from "enzyme";
 import toJson from "enzyme-to-json";
+
+import renderer from "react-test-renderer";
 
 import * as Media from "../src/components/media";
 
@@ -13,5 +15,28 @@ describe("HTML", () => {
     const component = mount(<Media.HTML data={"<b>cats are best</b>"} />);
     component.setProps({ data: "<b>squirrels are pretty great, too</b>" });
     expect(toJson(component)).toMatchSnapshot();
+  });
+});
+
+describe("<JSON />;", () => {
+  const population = {
+    total_population: [
+      { date: "2018-10-13", population: 329499086 },
+      { date: "2018-10-14", population: 329505562 }
+    ]
+  };
+
+  it("renders JSON data", () => {
+    const component = renderer
+      .create(<Media.JSON data={population} />)
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it("updates the theme if it changes", () => {
+    const component = renderer
+      .create(<Media.JSON data={population} theme={"dark"} />)
+      .toJSON();
+    expect(component).toMatchSnapshot();
   });
 });
--- packages/outputs/src/components/media/index.js
+++ packages/outputs/src/components/media/index.js
@@ -1,1 +1,2 @@
 export { HTML } from "./html";
+export { JSON } from "./json";
