7a450173e081f534a8cf5b68b3c02a3430478b4b
Safia Abdalla <safia@safia.rocks>
2018-10-15 20:17:34 -0500

    Add Media.SVG component (#3448)
    * Add Media.SVG component
    
    * Set mediaType as prop instead of static variable
    
    * Update snapshots

---
+++ packages/outputs/src/components/media/svg.js
@@ -0,0 +1,44 @@
+/* @flow */
+import React from "react";
+
+type Props = {
+  mediaType: string,
+  data: string
+};
+
+export class SVG extends React.Component<Props> {
+  el: ?HTMLElement;
+  static defaultProps = {
+    mediaType: "image/svg+xml",
+    date: ""
+  };
+
+  componentDidMount(): void {
+    if (this.el) {
+      this.el.insertAdjacentHTML("beforeend", this.props.data);
+    }
+  }
+
+  shouldComponentUpdate(nextProps: Props): boolean {
+    return nextProps.data !== this.props.data;
+  }
+
+  componentDidUpdate(): void {
+    if (!this.el) return;
+    // clear out all DOM element children
+    while (this.el.firstChild) {
+      this.el.removeChild(this.el.firstChild);
+    }
+    this.el.insertAdjacentHTML("beforeend", this.props.data);
+  }
+
+  render(): ?React$Element<any> {
+    return (
+      <div
+        ref={el => {
+          this.el = el;
+        }}
+      />
+    );
+  }
+}
